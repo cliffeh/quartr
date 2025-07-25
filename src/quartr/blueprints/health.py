94caa7ac3bff012e95fcf54725d811f85676d457
Cliff Snyder <cliff.snyder@gmail.com>
2025-07-25 15:22:33 -0400

    fixed env

--- src/quartr/blueprints/health.py
+++ src/quartr/blueprints/health.py
@@ -8,16 +8,16 @@ blueprint = Blueprint("health", __name__)
 @blueprint.route("/healthz", methods=["GET"], strict_slashes=False)
 async def healthz() -> dict[str, t.Any]:
     """A simple health check endpoint."""
-    return {"status": "ok", "env": current_app.config.get("QUARTR_ENV")}
+    return {"status": "ok", "env": current_app.config.get("ENV")}
 
 
 @blueprint.route("/livez", methods=["GET"], strict_slashes=False)
 async def livez() -> dict[str, t.Any]:
     """A simple liveness check endpoint."""
-    return {"status": "ok", "env": current_app.config.get("QUARTR_ENV")}
+    return {"status": "ok", "env": current_app.config.get("ENV")}
 
 
 @blueprint.route("/readyz", methods=["GET"], strict_slashes=False)
 async def readyz() -> dict[str, t.Any]:
     """A simple readiness check endpoint."""
-    return {"status": "ok", "env": current_app.config.get("QUARTR_ENV")}
+    return {"status": "ok", "env": current_app.config.get("ENV")}
