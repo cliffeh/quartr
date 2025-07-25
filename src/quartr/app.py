223afc4c9dc09274b971d25666cc5e0f85de9356
Cliff Snyder <cliff.snyder@gmail.com>
2025-07-24 21:22:55 -0400

    switch to using prefixed env

--- src/quartr/app.py
+++ src/quartr/app.py
@@ -6,9 +6,7 @@ from .hello import blueprint as hello_blueprint
 def create_app() -> Quart:
     # create and configure the app
     app = Quart(__name__)
-    app.config.from_mapping(
-        SECRET_KEY="dev",
-    )
+    app.config.from_prefixed_env("QUARTR")
 
     app.register_blueprint(hello_blueprint)
 
