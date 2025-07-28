c377ffa16c5068a8af565841a586b0edbd6f739b
Cliff Snyder <cliff.snyder@gmail.com>
2025-07-28 08:29:26 -0400

    basic static file/templates example

--- TODO.md
+++ TODO.md
@@ -2,7 +2,6 @@
 
 * Code that flexes async background jobs
 * Some stubbed-up bits around databases
-* Basic example code for static file handling
 * mechanism for injecting secret key?
 * get the Makefile to work for either local or devcontainer environments
 * integration tests that exercise an _actual_ running server
--- src/quartr/app.py
+++ src/quartr/app.py
@@ -1,4 +1,4 @@
-from quart import Quart
+from quart import Quart, render_template
 
 from .blueprints.health import blueprint as health_blueprint
 from .blueprints.hello import blueprint as hello_blueprint
@@ -6,8 +6,17 @@ from .blueprints.hello import blueprint as hello_blueprint
 
 def create_app() -> Quart:
     """Create and configure the quartr application."""
-    app = Quart(__name__)
+    app = Quart(
+        __name__,
+        static_url_path="",
+        static_folder="static",
+        template_folder="templates",
+    )
     app.config.from_prefixed_env("QUARTR")
+
+    @app.route("/")
+    async def index():
+        return await render_template("index.html")
 
     app.register_blueprint(health_blueprint)
     app.register_blueprint(hello_blueprint)
